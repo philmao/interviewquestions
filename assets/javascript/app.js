d746f4ec69959eece4f3930dd21dc842b1e45025
Nicholas Nociforo <Nicholas Nociforo>
2017-05-27 19:42:12 -0700

    ud

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,17 @@
-$.ajax({
-url: 'assets/json/html.json',
-type: 'GET',
 
-}).done(function(response) {
- console.log(response);
+// $.getJSON("html.json", function(json) {
 
-});
+
+
+
+// $.ajax({
+// url: 'assets/json/html.json',
+// type: 'GET',
+
+// }).done(function(response) {
+//  console.log(response);
+
+// });
 	
 
 
@@ -28,7 +34,21 @@ type: 'GET',
 
 
 
+ function loadJSON(callback) {   
 
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'assets/json/html.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ };
+
+ // console.log(xobj.responseText)
 
 
  
