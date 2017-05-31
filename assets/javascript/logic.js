75c5232de69374a194e341a8053c6bafc17ca05e
Nicholas Nociforo <Nicholas Nociforo>
2017-05-30 20:49:52 -0700

    updates

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -46,17 +46,17 @@ function generateHTML() {
 }});
 
 
-// $.ajax({
-//     type: "GET",
-//     url: "./assets/json/html.json",
-//     dataType: "json",
-//     success: function(result) {
-//         console.log(result);
-//     }
+$.ajax({
+    type: "GET",
+    url: "./assets/json/html.json",
+    dataType: "json",
+    success: function(result) {
+        console.log(result);
+    }
 
-// });
-
-
-$.getJSON("html.json", function(json) {
-    console.log(json); // this will show the info it in firebug console
 });
+
+
+// $.getJSON("html.json", function(json) {
+//     console.log(json); // this will show the info it in firebug console
+// });
