b7f696bf40bccfb7529bde23a0a8578830006e88
Kira Zoltan <kira@Kiras-MacBook-Pro.local>
2017-06-01 18:02:21 -0700

    update to page 2 and add link to 3

--- logic.js
+++ logic.js
@@ -19,9 +19,11 @@ $(document).ready(function() {
   $('body').on('click', '.btn-block', function(event){
     // $('.mainArea').hide();
     generateSecondHTML();
+
+  });
     
 
-  }); //closing onclick start-button event after generating new HTML page
+ //closing onclick start-button event after generating new HTML page
 
 
 // function for creation of second page with subject options
@@ -29,68 +31,71 @@ $(document).ready(function() {
 function generateSecondHTML() {
 
   
-  secondScreen = "<div class='container'><p>hi</p></div>";
-
-  //     // secondScreen = "<div class='selector'><ul><li><label for='css'>CSS</label><input type='checkbox'></li><li><label for='javascript'>JavaScript</label><input type='checkbox'></li><li><label for='html'>HTML</label><input type='checkbox'></li><li><label for='jquery'>JQuery</label><input type='checkbox'></li><li><label for=all'>ALL</label><input type='checkbox'></li></ul><div id='circle'>Choose a subject</div></div>";
-      
-      $('.secondMainArea').html(secondScreen);
-      $
+  window.location.href = "index2.html";
+    
 
 
       // $('.jumbotron').hide();
 
 }
 
+$('body').on('click', '.selector', function(event){
+    // $('.mainArea').hide();
+    generateThirdHTML();
+
+  });
+
+function generateThirdHTML() {
+
+  
+  window.location.href = "index3.html";
+    
+
+
+      // $('.jumbotron').hide();
+
+}
+
+
+
+
+
+
+
+// LOGIC FOR PAGE 2*****************************************************
+
+var angleStart = -360;
+
+// jquery rotate animation
+function rotate(li,d) {
+    $({d:angleStart}).animate({d:d}, {
+        step: function(now) {
+            $(li)
+               .css({ transform: 'rotate('+now+'deg)' })
+               .find('label')
+                  .css({ transform: 'rotate('+(-now)+'deg)' });
+        }, duration: 0
+    });
+}
+
+// show / hide the options
+function toggleOptions(s) {
+    $(s).toggleClass('open');
+    var li = $(s).find('li');
+    var deg = $(s).hasClass('half') ? 180/(li.length-1) : 360/li.length;
+    for(var i=0; i<li.length; i++) {
+        var d = $(s).hasClass('half') ? (i*deg)-90 : i*deg;
+        $(s).hasClass('open') ? rotate(li[i],d) : rotate(li[i],angleStart);
+    }
+}
+
+$('.selector button').click(function(e) {
+    toggleOptions($(this).parent());
 });
 
-
-// call second screen
-
+setTimeout(function() { toggleOptions('.selector'); }, 100);
 
 
+});
 
-
-// LOGIC FOR SECOND PAGE SYLING
-// *****************************************************************************
-
-//       var angleStart = -360;
-
-// // jquery rotate animation
-// function rotate(li,d) {
-//     $({d:angleStart}).animate({d:d}, {
-//         step: function(now) {
-//             $(li)
-//                .css({ transform: 'rotate('+now+'deg)' })
-//                .find('label')
-//                   .css({ transform: 'rotate('+(-now)+'deg)' });
-//         }, duration: 0
-//     });
-// }
-
-// // show / hide the options
-// function toggleOptions(s) {
-//     $(s).toggleClass('open');
-//     var li = $(s).find('li');
-//     var deg = $(s).hasClass('half') ? 180/(li.length-1) : 360/li.length;
-//     for(var i=0; i<li.length; i++) {
-//         var d = $(s).hasClass('half') ? (i*deg)-90 : i*deg;
-//         $(s).hasClass('open') ? rotate(li[i],d) : rotate(li[i],angleStart);
-//     }
-// }
-
-// $('.selector button').click(function(e) {
-//     toggleOptions($(this).parent());
-// });
-
-// setTimeout(function() { toggleOptions('.selector'); }, 100);
-
-
-
-// });
-
-
-
-   
-
-// END OF CREATION OF LOGIN PAGE AND GENERATION OF 2ND PAGE
 
