2d1eef68fbc71ce7bc4d644fd67b2cf656f283e4
philmao <philip_mao@yahoo.com>
2017-06-03 12:01:02 -0700

    update logic.js

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,11 +1,45 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD15_MQCpSIeLmOaujqowZkF-djMIi0UlY",
+    authDomain: "interviewquestions-ca991.firebaseapp.com",
+    databaseURL: "https://interviewquestions-ca991.firebaseio.com",
+    projectId: "interviewquestions-ca991",
+    storageBucket: "interviewquestions-ca991.appspot.com",
+    messagingSenderId: "139540148275"
+};
+firebase.initializeApp(config);
+
 // START COPY OF LOGIC.JS FILE
 // ******************************************************************
 $(document).ready(function() {
 
+    console.log(window.location.href);
+    // call initial start screen
+    if (window.location.href.match('index.html') != null) {
+        console.log("index.html ready");
+
+        initialScreen();
+    };
+    if (window.location.href.match('index3.html') != null) {
+        console.log("index3.html ready");
+        interviewQuestions.displayQuestion();
+
+
+        // interviewQuestions.displayQuestion(interviewQuestions.currentQuestion);
+    };
+});
+
+// on mouseclick the new HTML screen is generated
+$('body').on('click', '.btn-block', function(event){
+
+    // $('.mainArea').hide();
+    generateSecondHTML();
+
+});
+    
 
 // function for creation of initial start screen
-
-  function initialScreen() {
+function initialScreen() {
     startScreen = "<div class='container'><form class='form-signin'>"
     startScreen += "<div class='welcome'>Welcome!</div>"
     startScreen += "<h4 class='form-signin-heading'>Please sign in</h4>"
@@ -17,62 +51,38 @@ $(document).ready(function() {
     startScreen += "<button class='btn btn-lg btn-primary btn-block' type='submit'>Sign in</button></form></div>";
     $('.mainArea').html(startScreen);
 
-
-  }
-
-// call initial start screen
-
-  initialScreen ();
-
-// on mouseclick the new HTML screen is generated
-
-  $('body').on('click', '.btn-block', function(event){
-    // $('.mainArea').hide();
-    generateSecondHTML();
-
-  });
-    
-
- //closing onclick start-button event after generating new HTML page
-
+}
 
 // function for creation of second page with subject options
-
 function generateSecondHTML() {
 
   
-  window.location.href = "index2.html";
+    window.location.href = "index2.html";
     
-
-
-      // $('.jumbotron').hide();
+    // $('.jumbotron').hide();
 
 }
 
 $('body').on('click', '.selector', function(event){
+    console.log("click subject");
+    if(interviewQuestions.processSubject()){
+        generateThirdHTML();
+
+    }
+
     // $('.mainArea').hide();
-    generateThirdHTML();
 
   });
 
 function generateThirdHTML() {
 
-  
-  window.location.href = "index3.html";
-    
+    window.location.href = "index3.html";
+    console.log("switch to index3");
 
-
-      // $('.jumbotron').hide();
+    // $('.jumbotron').hide();
 
 }
 
-
-
-
-
-
-
-// LOGIC FOR PAGE 2*****************************************************
 
 var angleStart = -360;
 
@@ -105,47 +115,6 @@ $('.selector button').click(function(e) {
 
 setTimeout(function() { toggleOptions('.selector'); }, 100);
 
-
-
-
-// END COPY OF LOGIC.JS FILE
-// ********************************************************************
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyD15_MQCpSIeLmOaujqowZkF-djMIi0UlY",
-    authDomain: "interviewquestions-ca991.firebaseapp.com",
-    databaseURL: "https://interviewquestions-ca991.firebaseio.com",
-    projectId: "interviewquestions-ca991",
-    storageBucket: "interviewquestions-ca991.appspot.com",
-    messagingSenderId: "139540148275"
-};
-firebase.initializeApp(config);
-
-var database = firebase.database();
 
 //Setup linkedIn login
 var liLogin = function() { // Setup an event listener to make an API call once auth is complete
@@ -202,22 +171,20 @@ var myData;
 
 var interviewQuestions = {
 
-    indexNumber: 0,
-    maxTime: 30,
-    maxQuestions: 10,
+    // Results
     correctCount: 0,
     incorrectCount: 0,
     unansweredCount: 0,
+
+    // Timer initialization
     timer: 0,
+    maxTime: 30,
 
-
-
-
-
-
-
-
-
+    // Question var
+    currentQuestion: 0,
+    userAnswers: [],
+    maxQuestions: 10,
+    queryURL: "",
 
 
     // initialScreen: function() {
@@ -240,161 +207,179 @@ var interviewQuestions = {
     // },
     // displaySubject: function() {
     //     newHTML = "<p>Please choose a subject:</p>";
-    //     // newHTML += "<input type='button' class='btn btn-default btn-lg answerBtn' name='subject' id='html' value='HTML'>";
-    //     // newHTML += "<input type='button' class='btn btn-default btn-lg answerBtn' name='subject' id='css' value='CSS'>";
-    //     // newHTML += "<input type='button' class='btn btn-default btn-lg answerBtn' name='subject' id='javascript' value='Javascript'>";
-    //     // newHTML += "<input type='button' class='btn btn-default btn-lg answerBtn' name='subject' id='jQuery' value='JQuery'>";
-    //     newHTML += "<label><input type='radio' name='subject' id='html' value='html.json'>HTML</label><br>";
-    //     newHTML += "<label><input type='radio' name='subject' id='css' value='css.json'>CSS</label><br>";
-    //     newHTML += "<label><input type='radio' name='subject' id='javascript' value='javascript.json'>Javascript</label><br>";
-    //     newHTML += "<label><input type='radio' name='subject' id='jQuery' value='jquery.json'>JQuery</label><br>";
-    //     newHTML += "<button id='submitSubject' class='btn btn-lg btn-primary btn-block' type='submit'>Submit</button></form></div>";
+
+    //     newHTML += "<div class='btn-group-vertical' role='subject'>";
+
+    //     newHTML += "<button type='button' class='btn btn-default btn-lg subjectBtn' name='subject' id='html' value='html.json'>HTML</button>";
+    //     console.log (newHTML);
+    //     newHTML += "<button type='button' class='btn btn-default btn-lg subjectBtn' name='subject' id='css' value='css.json'>CSS</button>";
+    //     newHTML += "<button type='button' class='btn btn-default btn-lg subjectBtn' name='subject' id='javascript' value='javascript.json'>Javascript</button>";
+    //     newHTML += "<button type='button' class='btn btn-default btn-lg subjectBtn' name='subject' id='jQuery' value='jquery.json'>JQuery</button>";
+    //     newHTML += "</div>";
+
+    //     // newHTML += "<label><input type='radio' name='subject' id='html' value='html.json'>HTML</label><br>";
+    //     // newHTML += "<label><input type='radio' name='subject' id='css' value='css.json'>CSS</label><br>";
+    //     // newHTML += "<label><input type='radio' name='subject' id='javascript' value='javascript.json'>Javascript</label><br>";
+    //     // newHTML += "<label><input type='radio' name='subject' id='jQuery' value='jquery.json'>JQuery</label><br>";
+
+    //     newHTML += "<button id='submitSubject' class='btn btn-lg btn-primary btn-block' type='submit'>Submit</button></form>";
     //     $(".mainArea").html(newHTML);
 
     // },
 
-// $('body').on('click', '.btn-block', function(event){
-//     // $('.mainArea').hide();
-//     generateSecondHTML();
+    processSubject: function() {
 
-//   });
-    
+        var temp = $('input[type="checkbox"]:checked').val();
+        // var temp = $(".subjectBtn.active").val();
+        console.log(temp);
 
- //closing onclick start-button event after generating new HTML page
+        if(typeof temp == 'undefined') {
+            console.log("temp is undefined");
+            return false;
+        }
 
+        queryURL = "./assets/json/" + temp;
+        console.log(queryURL);
+        return true;
+    },
+    displayQuestion: function(questionNum) {
+        // $(".mainArea2").empty();
+        // console.log(myData);
+        // console.log(questionNum);
+        // var questionLine = $("<p>");
+        // questionLine.text(myData[questionNum].question);
+        // $(".mainArea2").append(questionLine);
+        // console.log(myData[questionNum].question);
 
-// function for creation of second page with subject options
+        // $(".mainArea2").append("<div class='btn-group-vertical' role='question'>");
 
-// generateSecondHTML: function() {
-
-  
-//   window.location.href = "index2.html";
-    
-
-
-      // $('.jumbotron').hide();
-
-}
-
-$('body').on('click', '.selector', function(event){
-    // $('.mainArea').hide();
-    generateThirdHTML();
-
-  });
-
-function generateThirdHTML() {
-
-  
-  window.location.href = "index3.html";
-    
+        // for(var i = 0; i < myData[questionNum].choices.length; i++) {
 
 
-      // $('.jumbotron').hide();
+        //     var answerChoice = "<button type='button' class='btn btn-default btn-lg addressBtn' ";
+        //     answerChoice += "value='" + parseInt(i + 1) + "'";  // value '0' is unanswered
+        //     answerChoice += " name='question" + parseInt(questionNum) + "'>";
+        //     answerChoice += myData[questionNum].choices[i];
+        //     answerChoice += "</button>";
+        //     $(".mainArea2").append(answerChoice);
+        //     console.log(answerChoice);
+        //     console.log(myData[questionNum].choices[i]);
+        // }
+        // $(".mainArea2").append("</div>");
 
-}
+        // if(questionNum === "0") {
+        //     $(".mainArea2").append("<button id='prevButton' class='btn btn-sm btn-primary btn-block'>Prev</button>");
+        // }
+        // if(questionNum === myData.length) {
+        //     $(".mainArea2").append("<button id='nextButton' class='btn btn-sm btn-primary btn-block'>Next</button>");
+        // }
 
+        $.ajax({
+            type: "GET",
+            url: queryURL,
+            dataType: "json",
+            success: function(result) {
+                myData = result.interview;
 
+                // $(".mainArea2").empty();
+                // console.log(myData);
+                // console.log(interviewQuestions.currentQuestion);
+                // var questionLine = $("<p>");
+                // questionLine.text(myData[interviewQuestions.currentQuestion].question);
+                // $(".mainArea2").append(questionLine);
+                // console.log(myData[interviewQuestions.currentQuestion].question);
 
+                // $(".mainArea2").append("<div class='btn-group-vertical' role='question'>");
 
-
-
-
-
-
-
-
-    // ORIGINAL JS BELOW******************************************
-    // processSubject: function() {
-
-    //     var temp = $('input[name="subject"]:checked').val();
-    //     console.log(temp);
-
-    //     var queryURL = "./assets/json/" + temp;
-    //     console.log(queryURL);
-
-    //     $.ajax({
-    //         type: "GET",
-    //         url: queryURL,
-    //         dataType: "json",
-    //         success: function(result) {
-    //             myData = result.interview;
-
-    //             $(".mainArea").empty();
-
-    //             for(var j = 0; j < result.interview.length; j++) {
-
-    //                 var questionLine = $("<p>");
-    //                 questionLine.text(result.interview[j].question);
-    //                 $(".mainArea").append(questionLine);
-    //                 console.log(result.interview[j].question);
-
-    //                 for(var i = 0; i < result.interview[j].choices.length; i++) {
-
-    //                     var answerChoice = $("<input>");
-    //                     answerChoice.attr("value", i + 1);  // value '0' is unanswered
-    //                     answerChoice.attr("type","radio");
-    //                     answerChoice.attr("name","question" + j);
-    //                     answerChoice.attr("class", "radioButtons");
-    //                     $(".mainArea").append(answerChoice);
-    //                     $(".mainArea").append("<b>" + result.interview[j].choices[i] + "</b><br>");
-
-    //                     // var answerChoice = $("<input class='btn btn-default btn-lg'>" + result.interview[j].choices[i] + "</button>");
-    //                     // answerChoice.attr("value", i + 1);  // value '0' is unanswered
-    //                     // answerChoice.attr("type","button");
-    //                     // answerChoice.attr("name","question" + j);
-    //                     // answerChoice.attr("class", "answerBtn");
-    //                     // $(".mainArea").append(answerChoice);
-    //                     // $(".mainArea").append("<b>" + result.interview[j].choices[i] + "</b><br>");
-
-    //                     console.log(result.interview[j].choices[i]);
-    //                 }
-    //             }
-    //             $(".mainArea").append("<button id='doneButton' class='btn btn-lg btn-primary btn-block'>Done</button>");
-
-    //         },
-    //         error: function(result){
-    //             console.log("Unable to get data");
-    //         }
-
-    //     });    
-    // },
-    // displayResults: function() {
-
-    //     // clearInterval(intervalId);
-    //     // $("#timer").text("");
-    //     console.log(myData);
-
-    //     console.log(myData.length);
-
-    //     for(i = 0; i < myData.length; i++) {
-
-    //         var name = "question" + i;
-    //         var temp = $('input[name="' + name + '"]:checked').val();
-    //         console.log(temp);
-    //         console.log(myData[i].correct);
-
-    //         if(isNaN(temp)) {
-    //             interviewQuestions.unansweredCount++;
-    //         }
-    //         else if (temp === myData[i].correct) {
-    //             interviewQuestions.correctCount++;
-    //         }
-    //         else {
-    //             interviewQuestions.incorrectCount++;
-    //         }
-    //     }
-
-    //     $(".mainArea").empty();
-    //     console.log("done");
-
-    //     $(".mainArea").append("<h2>All Done!</h2>");
-    //     $(".mainArea").append("<h2>Correct Answers: " + interviewQuestions.correctCount + "</h2>");
-    //     $(".mainArea").append("<h2>Incorrect Answers: " + interviewQuestions.incorrectCount + "</h2>");
-    //     $(".mainArea").append("<h2>Unanswered: " + interviewQuestions.unansweredCount + "</h2>");
-    //     $(".mainArea").append("<button id='resetButton' class='btn btn-lg btn-primary btn-block'>Reset</button>");
+                // for(var i = 0; i < myData[interviewQuestions.currentQuestion].choices.length; i++) {
 
 
-    // }
+                //     var answerChoice = "<button type='button' class='btn btn-default btn-lg addressBtn' ";
+                //     answerChoice += "value='" + parseInt(i + 1) + "'";  // value '0' is unanswered
+                //     answerChoice += " name='question" + parseInt(interviewQuestions.currentQuestion) + "'>";
+                //     answerChoice += myData[interviewQuestions.currentQuestion].choices[i];
+                //     answerChoice += "</button>";
+                //     $(".mainArea2").append(answerChoice);
+                //     console.log(answerChoice);
+                //     console.log(myData[interviewQuestions.currentQuestion].choices[i]);
+                // }
+                // $(".mainArea2").append("</div>");
+
+                // if(interviewQuestions.currentQuestion === "0") {
+                //     $(".mainArea2").append("<button id='prevButton' class='btn btn-sm btn-primary btn-block'>Prev</button>");
+                // }
+                // if(interviewQuestions.currentQuestion === myData.length) {
+                //     $(".mainArea2").append("<button id='nextButton' class='btn btn-sm btn-primary btn-block'>Next</button>");
+                // }
+
+                $(".mainArea").empty();
+                console.log(result.interview.length);
+                for(var j = 0; j < result.interview.length; j++) {
+
+                    var questionLine = $("<p>");
+                    questionLine.text(result.interview[j].question);
+                    $(".mainArea").append(questionLine);
+                    // console.log(result.interview[j].question);
+
+                    for(var i = 0; i < result.interview[j].choices.length; i++) {
+
+                        var answerChoice = $("<input>");
+                        answerChoice.attr("value", i + 1);  // value '0' is unanswered
+                        answerChoice.attr("type","radio");
+                        answerChoice.attr("name","question" + j);
+                        answerChoice.attr("class", "radioButtons");
+                        $(".mainArea").append(answerChoice);
+                        $(".mainArea").append("<b> " + result.interview[j].choices[i] + "</b><br>");
+
+                        // console.log(result.interview[j].choices[i]);
+                    }
+                }
+                $(".mainArea").append("<button id='doneButton' class='btn btn-lg btn-primary btn-block'>Done</button>");
+
+            },
+            error: function(result){
+                console.log("Unable to get data");
+            }
+            
+        });
+    },
+    displayResults: function() {
+
+        // clearInterval(intervalId);
+        // $("#timer").text("");
+        console.log(myData);
+
+        console.log(myData.length);
+
+        for(i = 0; i < myData.length; i++) {
+
+            var name = "question" + i;
+            var temp = $('input[name="' + name + '"]:checked').val();
+            console.log(temp);
+            console.log(myData[i].correct);
+
+            if(isNaN(temp)) {
+                interviewQuestions.unansweredCount++;
+            }
+            else if (temp === myData[i].correct) {
+                interviewQuestions.correctCount++;
+            }
+            else {
+                interviewQuestions.incorrectCount++;
+            }
+        }
+
+        $(".mainArea").empty();
+        console.log("done");
+
+        $(".mainArea").append("<h2>All Done!</h2>");
+        $(".mainArea").append("<h2>Correct Answers: " + interviewQuestions.correctCount + "</h2>");
+        $(".mainArea").append("<h2>Incorrect Answers: " + interviewQuestions.incorrectCount + "</h2>");
+        $(".mainArea").append("<h2>Unanswered: " + interviewQuestions.unansweredCount + "</h2>");
+        $(".mainArea").append("<button id='resetButton' class='btn btn-lg btn-primary btn-block'>Reset</button>");
+
+    }
+
     // decrement: function() {
 
     //     timer -= 1;
@@ -406,35 +391,12 @@ function generateThirdHTML() {
     //       interviewQuestions.displayResults();
     //     }
     // }
-// }
 
-// $(document).ready(function() {
 
-//     // call initial start screen
-//     interviewQuestions.initialScreen ();
-
-// });
+}
 
 
 
-// $("body").on("click", ".btn-block", function(event){
-
-//     event.preventDefault();
-
-//     interviewQuestions.generateSecondHTML();
-
-
-// }); 
-
-
-// $("body").on("click", "#submitSubject", function(event){
-
-//     event.preventDefault();
-
-//     interviewQuestions.processSubject(event); 
-
-
-// }); 
 
 // $("body").on("click", "#doneButton", function(event){
 
@@ -457,5 +419,35 @@ function generateThirdHTML() {
 
 
 // }); 
+
+$("body").on("click", ".subjectBtn", function(event){
+
+    $('.btn-group-vertical > .btn').removeClass('active');
+    $(this).addClass('active');
+    console.log("subject button selected");
+
+});
+
+$("body").on("click", ".addressBtn", function(event){
+
+    $('.btn-group-vertical > .btn').removeClass('active');
+    $(this).addClass('active');
+    console.log("address button selected");
+
+});
+
+$("body").on("click", ".prevButton", function(event){
+
+    interviewQuestions.currentQuestion--;
+    interviewQuestions.displayQuestion(interviewQuestions.currentQuestion);
+    console.log("prev button pressed");
+
+});
+
+$("body").on("click", ".nextButton", function(event){
+
+    interviewQuestions.currentQuestion++;
+    interviewQuestions.displayQuestion(interviewQuestions.currentQuestion);
+    console.log("next button pressed");
 
 });
